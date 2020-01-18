e1bbc962b49e2dc8dbbbee5f17ae08e9cb6bd5a9
Nimisha Wilson <nimishawilson123@gmail.com>
2020-01-18 10:46:22 +0530

    hadling errors of observables

--- observables/src/app/home/home.component.ts
+++ observables/src/app/home/home.component.ts
@@ -22,11 +22,17 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(() => {
         observer.next(count);
+        if(count > 3){
+          observer.error(new Error( 'count is greater than 3'))
+        }
         count++;
       }, 1000);
     });
   this.firstObsSubscription =  customIntervalObservable.subscribe(data => {
       console.log(data);
+    }, error => {
+      console.log(error);
+      alert(error.message);
     })
   }
 
