fd7f7492712ec7c769911578d06583b95070aaae
Nimisha Wilson <nimishawilson123@gmail.com>
2020-01-20 21:08:59 +0530

    getting form data to component page

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -10,6 +10,8 @@ export class AppComponent {
   @ViewChild('f', {static: false}) signupForm: NgForm;
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.submitted  = false;
+    
     // this.signupForm.setValue({
     //   userData: {
     //     username: suggestedName,
@@ -29,12 +31,24 @@ export class AppComponent {
   defaultQuestion = "teacher";
   answer = '';
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
 
   // onSubmit(form: NgForm){
   //   console.log('submitted');
   //   console.log(form);
   // }
   onSubmit(){
-    console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
   }
 }
