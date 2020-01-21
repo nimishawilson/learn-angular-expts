6576cb3b2f5574862cbe67e3df83cbe03671afec
Nimisha Wilson <nimishawilson123@gmail.com>
2020-01-21 15:23:00 +0530

    initialize form and create controls

--- forms-reactive/src/app/app.module.ts
+++ forms-reactive/src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -11,7 +9,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    ReactiveFormsModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
