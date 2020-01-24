c5558fa8c588a325529d1f268028e60b26a833d7
Nimisha Wilson <nimishawilson123@gmail.com>
2020-01-24 17:56:51 +0530

    add impure pipe

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -40,4 +40,13 @@ export class AppComponent {
       'list-group-item-danger': server.status === 'critical'
     };
   }
+onAddServer(){
+  this.servers.push({
+    instanceType: 'small',
+    name: 'new server',
+    status: 'stable',
+    started: new Date(24, 1, 2020)
+  });
+}
+
 }
